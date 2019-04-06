b21d782d0a4fd3e9c282c0c3aaa34a1decefbe13
Quoc Huy Nguyen <a1761672@student.adelaide.edu.au>
2019-04-06 23:21:30 +1030

    Initial data services project

---
+++ sensor-data-services/sensor-data-services.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import redis
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+_redis = redis.StrictRedis(host="localhost", port=6379, db=0)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+class DeviceList(Resource):
+    def get(self):
+        device_list = _redis.lrange("device_list", 0, 100)
+        data_dict = {}
+        tmp_devices = []
+        for device in device_list:
+            tmp_devices.append(device.decode('utf-8'))
+        data_dict["device_list"] = tmp_devices
+        print(device_list)
+        return data_dict
+
+class SensorTagInfo(Resource):
+    def get(self, device_id):
+        device_info = _redis.lrange(device_id, 0, 100)
+        data_dict = {}
+        tmp_devices = []
+        for device in device_info:
+            tmp_devices.append(json.loads(device))
+        data_dict["data"] = tmp_devices
+        # print(tmp_devices)
+        return data_dict
+
+api.add_resource(DeviceList, '/devices')
+api.add_resource(SensorTagInfo, '/device/info/all/<string:device_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
