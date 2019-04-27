b0f34819f0bcaca08e7c51a1aade08d392b23cf5
Quoc Huy Nguyen <a1761672@student.adelaide.edu.au>
2019-04-28 04:23:59 +0930

    Refactor code, create new thread for searching sensors every 30 secs.

--- sensor-data-collector/SensorTagDetector.py
+++ sensor-data-collector/SensorTagDetector.py
@@ -6,52 +6,63 @@ import threading
 import time
 import json
 import redis
+from threading import Timer, Thread, Event
+
 
 def main():
-    import time
-    import sys
-    import argparse
+    interval = 30
+    stopFlag = Event()
+    timerTask = TimerTask(stopFlag, interval)
+    timerTask.start()
 
-    scanner = btle.Scanner()
-    device_list = []
 
-    duration = 10.0
-    current_device_id = 0
-    count_test = 0
-    _redis = redis.StrictRedis(host="localhost", port=6379, db=0)
-    print("Try to detect sensors each %s seconds" %duration)
-    device_info_list = scanner.scan(5)
+class TimerTask(Thread):
+    def __init__(self, event, duration):
+        Thread.__init__(self)
+        self.stopped = event
+        self.duration = duration
 
-    for device in device_info_list:
-        device_name = device.getValueText(9)
-        if (device_name is not None) and ("SensorTag" in device_name):
-            print(device.addr)
-            if len(device_list) == 0:
-                data_thread = RetrievingDataThread(current_device_id, "Thread - " + str(current_device_id),
-                                                   current_device_id, device)
-                current_device_id += 1
-                device_list.append(device.addr)
-                _redis.lpush("device_list", device.addr)
-                print(device_list)
-                data_thread.start()
-            else:
-                not_existed = True
-                for tmp_run in device_list:
-                    if device.addr == tmp_run:
-                        not_existed = False
-                print(device_list)
-                if not_existed:
-                    device_list.append(device.addr)
-                    _redis.lpush("device_list", device.addr)
-                    data_thread = RetrievingDataThread(current_device_id, "Thread - " + str(current_device_id),
-                                                       current_device_id, device)
-                    current_device_id += 1
-                    data_thread.start()
-    time.sleep(duration)
-    # time.sleep(10)
-    # scanner = btle.Scanner()
-    # device_info_list = scanner.scan(5)
+    def run(self):
 
+        while not self.stopped.wait(self.duration):
+            try:
+                device_list = []
+                current_device_id = 0
+                scanner = btle.Scanner()
+
+                _redis = redis.StrictRedis(host="localhost", port=6379, db=0)
+                print("Try to detect sensors each %s seconds" % self.duration)
+                device_info_list = scanner.scan(5)
+
+                for device in device_info_list:
+                    device_name = device.getValueText(9)
+                    print("Device addr ", device.addr, "name " , device_name)
+
+                    if (device_name is not None) and ("SensorTag" in device_name):
+
+                        if len(device_list) == 0:
+                            data_thread = RetrievingDataThread(current_device_id, "Thread - " + str(current_device_id),
+                                                               current_device_id, device)
+                            current_device_id += 1
+                            device_list.append(device.addr)
+                            data_thread.start()
+                        else:
+                            not_existed = True
+                            for tmp_run in device_list:
+                                if device.addr == tmp_run:
+                                    not_existed = False
+
+                            if not_existed:
+                                device_list.append(device.addr)
+                                data_thread = RetrievingDataThread(current_device_id, "Thread - " + str(current_device_id),
+                                                                   current_device_id, device)
+                                current_device_id += 1
+                                data_thread.start()
+                for tmp in device_list:
+                    _redis.sadd("device_list", tmp)
+            except Exception as e:
+                print("Exception in scanning")
+                print(e)
 
 class RetrievingDataThread(threading.Thread):
     _sensor_info = None
@@ -68,41 +79,38 @@ class RetrievingDataThread(threading.Thread):
         self._sensor_info = sensor_info
 
         self._redis = redis.StrictRedis(host="localhost", port=6379, db=0)
-
     def run(self):
         print(">>>>>...")
         print(self._sensor_info)
         self._reconnect()
+
         while not self._exit_flag and self._retry_count < 5:
             try:
-                # print('Temp: ', self._sensor.IRtemperature.read())
-                # print("Humidity: ", self._sensor.humidity.read())
-                # print("Barometer: ", self._sensor.barometer.read())
-                # print("Accelerometer: ", self._sensor.accelerometer.read())
-                # print("Magnetometer: ", self._sensor.magnetometer.read())
-                # print("Gyroscope: ", self._sensor.gyroscope.read())
-                # print("Light: ", self._sensor.lightmeter.read())
-                # Get the timestamp
-                current_time = time.time()
-                data_dict = {}
-                data_dict["current_time"] = current_time
-                data_dict["MAC"] = self._sensor_info.addr
-                data_dict["temperature"] = self._sensor.IRtemperature.read()
-                data_dict["humidity"] = self._sensor.humidity.read()
-                data_dict["barometer"] = self._sensor.barometer.read()
-                data_dict["accelerometer"] = self._sensor.accelerometer.read()
-                data_dict["magnetometer"] = self._sensor.magnetometer.read()
-                data_dict["gyroscope"] = self._sensor.gyroscope.read()
-                data_dict["light"] = self._sensor.lightmeter.read()
-                data_dict["battery"] = self._sensor.battery.read()
-                json_str = json.dumps(data_dict)
-                length = self._redis.lpush(self._sensor_info.addr, json_str)
-                if length > 100:
-                    self._redis.rpop(self._sensor_info.addr)
-                print(json_str)
-
+                if self._sensor.getState() == "conn":
+                    # Get the timestamp
+                    current_time = time.asctime(time.localtime(time.time()))
+                    data_dict = {}
+                    data_dict["current_time"] = current_time
+                    data_dict["MAC"] = self._sensor_info.addr
+                    data_dict["temperature"] = self._sensor.IRtemperature.read()
+                    data_dict["humidity"] = self._sensor.humidity.read()
+                    data_dict["barometer"] = self._sensor.barometer.read()
+                    data_dict["accelerometer"] = self._sensor.accelerometer.read()
+                    data_dict["magnetometer"] = self._sensor.magnetometer.read()
+                    data_dict["gyroscope"] = self._sensor.gyroscope.read()
+                    data_dict["light"] = self._sensor.lightmeter.read()
+                    data_dict["battery"] = self._sensor.battery.read()
+                    json_str = json.dumps(data_dict)
+                    length = self._redis.lpush(self._sensor_info.addr, json_str)
+                    if length > 100:
+                        self._redis.rpop(self._sensor_info.addr)
+                    #print(json_str)
+                else:
+                    self._retry_count += 1
+                    if self._retry_count < 5:
+                        self._reconnect()
             except Exception as e:
-                print("Exception, try to connect again")
+                print("Exception, wait and try to reconnect")
                 print(e)
                 self._retry_count += 1
                 if self._retry_count < 5:
@@ -114,12 +122,13 @@ class RetrievingDataThread(threading.Thread):
         self._exit_flag = exit_flag
 
     def _reconnect(self):
-        if self._sensor is not None:
-            self._sensor.disconnect()
-            del self._sensor
+        try:
+            if self._sensor is not None:
+                self._sensor.disconnect()
+                self._sensor = None
 
-        if self._sensor_info is not None:
-            try:
+            if self._sensor is None and self._sensor_info is not None:
+
                 print("Try to connect SensorTag...")
                 self._sensor = sensortag.SensorTag(self._sensor_info.addr)
 
@@ -135,10 +144,10 @@ class RetrievingDataThread(threading.Thread):
                 self._sensor.lightmeter.enable()
                 self._sensor.battery.enable()
 
-                time.sleep(1.0)
-                self._retry_count = 0
-            except Exception as e:
-                print(e)
+            time.sleep(1.0)
+            self._retry_count = 0
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
