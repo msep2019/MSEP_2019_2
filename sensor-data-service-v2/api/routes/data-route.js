5cfca72e92355a72608a4ca3272eb320cf7ba368
conglincolin <48201804+conglincolin@users.noreply.github.com>
2019-08-08 22:59:42 +0930

    Update data-route.js

--- sensor-data-service-v2/api/routes/data-route.js
+++ sensor-data-service-v2/api/routes/data-route.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = function(app) {
     var data_stream_controller = require('../controllers/data-stream-controller');
+    //var data_controllers = require('../controllers/data-controller');
   
     // stream routes
     app.route('/stream')
