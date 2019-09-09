831df0300e15ccdc872f181c1026879268dafb25
Quoc-Huy-Nguyen <a1761672@student.adelaide.edu.au>
2019-09-09 10:32:50 +0930

    Add new method to store datastream name into contract

--- huy-nguyen/event-trigger-example/contracts/SimpleContract.sol
+++ huy-nguyen/event-trigger-example/contracts/SimpleContract.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 contract SimpleContract {
    event SimpleEvent(address from, uint a, uint b, uint result);
-
+   string public datastream;
    function add(uint a, uint b) private pure returns (uint){
        uint result;
        result = a + b;
@@ -12,5 +12,9 @@ contract SimpleContract {
       uint result;
       result = add(a, b);
       emit SimpleEvent(msg.sender, a, b, result);
+   }
+
+   function storeDatastream(string memory name) public {
+      datastream = name;
    }
 }
